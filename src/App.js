93e7a7261c474532148ba0e8b32c1ecd5da8971c
Dexter <dexterdevmode@gmail.com>
2022-02-11 11:11:46 +0530

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Dexter</div>
+      <div className="content">developer</div>
+    </div>
+  );
+}
+
+export default App;
