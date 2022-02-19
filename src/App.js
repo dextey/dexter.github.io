ffc5a762d7a29ebcba2cb47e14456c9374be7284
Dexter <dexterdevmode@gmail.com>
2022-02-19 22:36:46 +0530

    Contact has been added using mailto

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
 import AboutMe from "./Components/AboutMe";
 import Intro from "./Components/Intro";
 import Skill from "./Components/Skill";
-import {
-  Animator,
-  ScrollContainer,
-  ScrollPage,
-  batch,
-  Fade,
-  FadeIn,
-  Move,
-  MoveIn,
-  MoveOut,
-  Sticky,
-  StickyIn,
-  ZoomIn,
-  ZoomOut,
-} from "react-scroll-motion";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
     <>
-      <ScrollContainer>
-        <ScrollPage page={0}>
-          <Animator animation={batch(Fade(), MoveOut(0, -600))}>
-            <Intro />
-          </Animator>
-        </ScrollPage>
-      </ScrollContainer>
-      <ScrollContainer>
-        <ScrollPage page={1}>
-          <Animator animation={batch(Fade())}>
-            <div className="bg-slate-800 h-[100vh]">
-              <Animator animation={batch(Fade(), ZoomIn())}>
-                <div className="m-4">
-                  <div className=" text-white text-center font-extrabold text-5xl pt-28 m-4">
-                    About Me
-                  </div>
-                </div>
-              </Animator>
-              <Animator animation={batch(Fade())}>
-                <div>
-                  <AboutMe />
-                </div>
-              </Animator>
-            </div>
-          </Animator>
-        </ScrollPage>
-      </ScrollContainer>
-      <ScrollContainer>
-        <ScrollPage page={2}>
-          <Animator animation={ZoomIn()}>
-            <div className="m-4">
-              <div className=" text-white sm:ml-20 font-extrabold text-5xl pt-28 m-4">
-                Skills
-              </div>
-            </div>
-          </Animator>
-          <Animator animation={batch(Fade(), MoveIn(-2000, 0))}>
-            <Skill />
-          </Animator>
-        </ScrollPage>
-      </ScrollContainer>
+      <div className="mail flex justify-center items-center rounded-full bg-yellow-300">
+        <a href="mailto:dexterdevmode@gmail.com">
+          <img src="mail.png" alt="mail" />
+        </a>
+      </div>
+      <section>
+        <Intro />
+      </section>
+      <section>
+        <AboutMe />
+      </section>
+      <section>
+        <Skill />
+      </section>
+      <section>
+        <Projects />
+      </section>
     </>
   );
 }
