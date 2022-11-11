ef541e3cfe07dfae4752a992baa7aced06b83bd3
Dexter <dexterdevmode@gmail.com>
2022-11-11 23:04:25 +0530

    updated colors and animations

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="w-full flex justify-between h-[50px] bg-slate-500">
+    <div className="w-full flex justify-between h-[50px] bg-slate-700">
       <div className="m-3  text-3xl font-extrabold text-yellow-200 font-fest ">
         <Link to={"/"}> Dexter</Link>
       </div>
