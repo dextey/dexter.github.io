beaf66dc405e28b4a0164a2efe8e990ae9a3571a
dexter <dexterdevmode@gmail.com>
2024-03-04 21:18:01 +0530

    updated blog intro

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Poppins } from "next/font/google";
 const poppins = Poppins({ weight: ["100", "200", "400", "700", "900"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dexter",
+  title: "dextey",
   description: "A software enginner &, a developer who is passionated about adding bits on internet",
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { getBlogs } from "@/data/blogs";
-import { formatDate, formatDateString } from "@/utils/utils";
-import axios from "axios";
+import { formatDateString } from "@/utils/utils";
 import Link from "next/link";
 import { FaGithub, FaTwitter } from "react-icons/fa";
-import { SiHashnode } from "react-icons/si";
-import { useQuery } from "react-query";
 
 export default async function Home() {
   const blogs = getBlogs();
@@ -12,7 +9,7 @@ export default async function Home() {
   return (
     <div className="w-full bg-[#303034] text-white text-sm sm:text-xl ">
       <div className="container mx-auto flex flex-col  h-screen overflow-y-scroll pt-5 px-5">
-        <nav className=" flex justify-between px-4 py-4 bg-yellow-100 text-black items-center ">
+        <nav className=" lg:mx-5 flex justify-between px-4 py-4 bg-yellow-100 text-black items-center ">
           <div className="font-bold "> $ :&gt; /dev/dump 💬</div>
           <div className="flex gap-5 items-center">
             <Link href={"/about"} className="contact hover:after:bg-yellow-100">
@@ -21,9 +18,14 @@ export default async function Home() {
           </div>
         </nav>
 
-        <section className="container flex px-7 pt-7 text-2xl gap-3 flex-col">
-          <span className="text-sm md:text-md lg:text-2xl font-mono ">
-            hey there,<span className="w-2 bg-yellow-100  h-2 text-yellow-100 animate-pulse">:</span>
+        <section className="container flex px-5  md:px-7 pt-7 text-2xl gap-3 flex-col">
+          <span className="text-sm md:text-md lg:text-2xl font-mono lg:pb-4 ">
+            <span className="rotate-6 font-bold text-2xl lg:text-5xl font-sans bg-clip-text text-transparent  bg-gradient-to-tr from-indigo-500 via-rose-300  to-yellow-200   ">
+              hey there,
+            </span>
+            <br />
+            <span>random thoughts and learnings</span>
+            <span className=" mx-1 w-2 bg-yellow-100  h-2 text-yellow-100 animate-pulse">:</span>
           </span>
           <div className="flex gap-2 text-sm sm:text-lg lg:text-2xl text-yellow-100">
             <Link target="_blank" href="http://github.com/dextey">
@@ -38,8 +40,8 @@ export default async function Home() {
         <section className="flex flex-1 text-base">
           <div className="font-mono  border-r-2  hidden lg:flex flex-col w-1/6  my-8 py-4 items-center gap-2 border-[#3d3d3d] ">
             <div className="flex flex-col gap-2 font-bold">
-              <span className="hover:bg-yellow-100 hover:text-black px-4 py-1">#featured</span>
-              <span className="hover:bg-yellow-100 hover:text-black px-4 py-1">#weekly</span>
+              <span className="hover:bg-yellow-100 hover:text-black px-4 py-1 cursor-pointer">#featured</span>
+              <span className="hover:bg-yellow-100 hover:text-black px-4 py-1 cursor-pointer">#weekly</span>
             </div>
           </div>
           <div className="flex w-full flex-col lg:p-5">
